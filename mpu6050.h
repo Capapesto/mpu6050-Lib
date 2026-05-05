6a44baf59935e7dc617fc366858365126f544955
Andrey André <capstolisto@gmail.com>
2026-05-05 15:34:09 -0300

    Update mpu6050.h

--- mpu6050.h
+++ mpu6050.h
@@ -12,4 +12,19 @@
 
 
 
+// ================= CONFIG =================
+
+#define MPU6050_ADDR         0x68
+#define I2C_MASTER_SCL_IO    22
+#define I2C_MASTER_SDA_IO    21
+#define I2C_MASTER_NUM       I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ   400000
+
+#define QUEUE_SIZE 10
+
+
+
+
+
+
 #endif 
