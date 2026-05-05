f6bf2f266fcf3308b0da93a6b6010837c19c9559
Andrey André <capstolisto@gmail.com>
2026-05-05 15:33:13 -0300

    Update mpu6050.h

--- mpu6050.h
+++ mpu6050.h
@@ -1,9 +1,14 @@
 #ifndef MPU6050_H
 #define MPU6050_H
 
+#include <stdio.h>
+#include <math.h>
 #include "driver/i2c.h"
-#include "esp_err.h"
-
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_timer.h"
 
 
 
